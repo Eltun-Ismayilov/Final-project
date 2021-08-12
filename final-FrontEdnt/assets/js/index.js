bc15cd8548dbfacc54f201fd3572c0b551369632
Ismayilov Eltun <eltunhi@code.local>
2021-08-12 21:31:20 +0400

    calculator

--- final-FrontEdnt/assets/js/index.js
+++ final-FrontEdnt/assets/js/index.js
@@ -240,3 +240,16 @@ if (rangeElement) {
 }
 
 
+function updateTextInput(val) {
+  document.getElementById('textInput').value=val; 
+}
+function updateTextInput1(val) {
+  document.getElementById('textInput2').value=val; 
+}
+function updateTextInput2(val) {
+  document.getElementById('textInput3').value=val; 
+}
+function updateTextInput3(val) {
+  document.getElementById('textInput4').value=val; 
+}
+
